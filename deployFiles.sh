63e8a1cab680f06e43e42a68f4f750a3b5d9a525
ethanmchls <ethanmchls@gmail.com>
2023-02-21 20:32:37 -0700

    updated styling

--- deployFiles.sh
+++ deployFiles.sh
@@ -9,21 +9,21 @@ do
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
-    printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
-    exit 1
-fi
+# if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
+#     printf "\nMissing required parameter.\n"
+#     printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+#     exit 1
+# fi
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i $key ubuntu@$hostname << ENDSSH
-rm -rf services/${service}/public
-mkdir -p services/${service}/public
+ssh -i /Users/ethanmchls/Desktop/Programming-stuff/cs260/260aws.pem ubuntu@ethanmchls.click << ENDSSH
+rm -rf services/simon/public
+mkdir -p services/simon/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i $key * ubuntu@$hostname:services/$service/public
+scp -r -i /Users/ethanmchls/Desktop/Programming-stuff/cs260/260aws.pem * ubuntu@ethanmchls.click:services/simon/public
